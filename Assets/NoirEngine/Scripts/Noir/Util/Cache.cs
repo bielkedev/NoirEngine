3d854a0ad7cb298ce4e0237a1d872b55a7c67716
AcrylicShrimp <led789zxpp@naver.com>
2016-11-16 16:59:55 +0900

    1.Android build test complete.

---
+++ Assets/NoirEngine/Scripts/Noir/Util/Cache.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Noir.Unity
+{
+	public class Cache<K, V>
+	{
+		public delegate void DisposeValue(V sValue);
+
+		private DisposeValue fDisposeValue;
+		private Dictionary<K, V> sCacheMap;
+
+		public Cache(DisposeValue fNewDisposeValue)
+		{
+			this.fDisposeValue = fNewDisposeValue;
+			this.sCacheMap = new Dictionary<K, V>();
+		}
+
+		public V this[K sKey]
+		{
+			get
+			{
+				V sValue;
+				this.sCacheMap.TryGetValue(sKey, out sValue);
+				return sValue;
+			}
+			set { this.sCacheMap.Add(sKey, value); }
+		}
+
+		public void addCache(K sKey, V sValue)
+		{
+			this.sCacheMap.Add(sKey, sValue);
+		}
+
+		public void releaseCache(K sKey)
+		{
+			this.fDisposeValue(this[sKey]);
+		}
+
+		public void releaseCacheAll()
+		{
+			foreach (var sPair in this.sCacheMap)
+				this.fDisposeValue(sPair.Value);
+
+			this.sCacheMap.Clear();
+		}
+	}
+}
